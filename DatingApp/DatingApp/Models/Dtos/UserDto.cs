deba6cebe829998bd54cfec5a3a060ddc2003faf
xahir509 <xahirahmed3@gmail.com>
2021-02-03 02:22:28 +0600

    Use Validation in Dto Model

--- DatingApp/DatingApp/Models/Dtos/UserDto.cs
+++ DatingApp/DatingApp/Models/Dtos/UserDto.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.Models.Dtos
 {
   public class UserDto
   {
     public long Id { get; set; }
+    [Required]
     public string Username { get; set; }
+    [Required]
+    [StringLength(8,MinimumLength = 4,ErrorMessage = "Password must be between 4 to 8 character")]
     public string Password { get; set; }
   }
 }

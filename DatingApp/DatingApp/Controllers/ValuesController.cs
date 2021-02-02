b47e6257f839f72c1a5ca659b91fa7405d2787ac
xahir509 <xahirahmed3@gmail.com>
2021-02-02 23:41:39 +0600

    TestController-Value Checked Backend

---
+++ DatingApp/DatingApp/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.Data;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        
+
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _context.Values.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("Detail/{id}")]
+        public IActionResult Detail(int id)
+        {
+            var value = _context.Values.FirstOrDefault(c=>c.Id==id);
+            return Ok(value);
+        }
+    }
+}

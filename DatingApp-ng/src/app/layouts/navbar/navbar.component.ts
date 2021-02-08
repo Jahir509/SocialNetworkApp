949de396569101d9e53ae69ad252ad899eb1b833
xahir509 <xahirahmed3@gmail.com>
2021-02-09 00:29:47 +0600

    Add: Navbar component

---
+++ DatingApp-ng/src/app/layouts/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../_services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(private _authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this._authService.login(this.model).subscribe((data) => {
+        console.log(('logged in service'))
+      },
+      error => {
+        console.log('failed to login')
+      });
+  }
+
+}

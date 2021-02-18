d73a849878175dc89aceea9e1444f86a3f57569e
xahir509 <xahirahmed3@gmail.com>
2021-02-19 00:06:16 +0600

    Photo and User Model Added

--- DatingApp/DatingApp/Models/User.cs
+++ DatingApp/DatingApp/Models/User.cs
@@ -1,10 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DatingApp.Policies;
+
 namespace DatingApp.Models
 {
-  public class User
+  public class User:ILog,IAddress
   {
     public long Id { get; set; }
     public string Username { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public string Gender { get; set; }
+    public string KnownAs { get; set; }
+    public string Introduction { get; set; }
+    public string LookingFor { get; set; }
+    public string Interests { get; set; }
+    public ICollection<Photo> Photos { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+
+    // Contracts ------------------------------
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime LastActive { get; set; }
+    public string? HouseNo { get; set; }
+    public string? RoadNo { get; set; }
+    public string Area { get; set; }
+    public string City { get; set; }
+    public string? Zip { get; set; }
+    public string Country { get; set; }
   }
 }

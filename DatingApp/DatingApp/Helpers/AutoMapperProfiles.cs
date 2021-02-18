7738e6b06ebcc06402533c93c9d516b09da74d2d
xahir509 <xahirahmed3@gmail.com>
2021-02-19 00:04:13 +0600

    Automapper Updated

---
+++ DatingApp/DatingApp/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.Models;
+using DatingApp.Models.Dtos;
+
+namespace DatingApp.Helpers
+{
+  public class AutoMapperProfiles : Profile
+  {
+    public AutoMapperProfiles()
+    {
+      CreateMap<User, UserDto>()
+        .ForMember(dest => dest.PhotoUrl,
+          opt => opt.MapFrom(
+            src => src.Photos.FirstOrDefault(p => p.IsMain == true).Url))  //Adding Photo Property from User to UserDto Model
+        .ForMember(dest => dest.Age,
+          opt => opt.MapFrom(
+            src => src.DateOfBirth.CalculateAge()));  //Adding Age Property from User to UserDto Model Using DateTime
+
+
+      CreateMap<User, UserDetailsDto>()
+        .ForMember(dest => dest.PhotoUrl, 
+          opt => opt.MapFrom(
+            src => src.Photos.FirstOrDefault(p => p.IsMain == true).Url))   //Adding Photo Property from User to UserDetailsDto Model
+        .ForMember(dest => dest.Age,
+          opt => opt.MapFrom(
+            src => src.DateOfBirth.CalculateAge()));    //Adding Age Property from User to UserDetailsDto Model Using DateTime
+
+
+      CreateMap<Photo, PhotoDetailsDto>();
+    }
+  }
+}

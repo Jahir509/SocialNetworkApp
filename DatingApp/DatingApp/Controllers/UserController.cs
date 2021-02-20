57a2cf01d7e6a379197a1ce28ed0dc4330f8938c
xahir509 <xahirahmed3@gmail.com>
2021-02-20 23:51:07 +0600

    User controller Change

--- DatingApp/DatingApp/Controllers/UserController.cs
+++ DatingApp/DatingApp/Controllers/UserController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.Controllers
 {
-
+  [Authorize]
   [ApiController]
   [Route("api/[controller]")]
   public class UserController : ControllerBase

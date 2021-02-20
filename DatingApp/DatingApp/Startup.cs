bf68db31db16320301a39f3a9a07111f9f612cb1
xahir509 <xahirahmed3@gmail.com>
2021-02-20 23:54:42 +0600

    Update Startuo :)

--- DatingApp/DatingApp/Startup.cs
+++ DatingApp/DatingApp/Startup.cs
@@ -1,22 +1,20 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using DatingApp.Data;
 using DatingApp.Helpers;
+using DatingApp.Repository;
 using DatingApp.Repository.Auth;
+using DatingApp.Repository.Contracts;
 using DatingApp.Repository.Contracts.IAuth;
+using DatingApp.Service;
 using DatingApp.Service.AuthService;
+using DatingApp.Service.Contracts;
 using DatingApp.Service.Contracts.IAuthService;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -39,10 +37,18 @@ namespace DatingApp
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddDbContext<DataContext>(d => d.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-      services.AddControllers();
+      services.AddControllers()
+        .AddNewtonsoftJson(opt=>  
+          opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+          );  // NewtonSoftJson For Cycling Dependency Error Solve
       services.AddCors();
+      services.AddAutoMapper(typeof(UserRepository).Assembly);
       services.AddScoped<IAuthRepository, AuthRepository>();
       services.AddScoped<IAuthService, AuthService>();
+      services.AddScoped<IUserRepository, UserRepository>();
+      services.AddScoped<IUserService, UserService>();
+      services.AddScoped<IDatingRepository, DatingRepository>();
+      // JWT Authentication Middleware Application Here
       services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         .AddJwtBearer(options =>
       {

4b8335bc163b06eb3545724cc8cdc13e856c500a
xahir509 <xahirahmed3@gmail.com>
2021-02-20 23:53:54 +0600

    Frontend Model Added

---
+++ DatingApp-ng/src/app/_guards/_models/user.ts
@@ -0,0 +1,27 @@
+import { ILog } from './_interfaces/ILog';
+import { IAddress } from './_interfaces/IAddress';
+import { Photo } from './photo';
+
+export class User implements ILog,IAddress{
+  id:number;
+  username:string;
+  knownAs:string;
+  age:number;
+  gender:number;
+  photoUrl:string;
+  introduction?:string;
+  interests?:string;
+  photos?: Photo[];
+  // Contracts -----------
+  area: string;
+  city: string;
+  country: string;
+  createdAt: Date;
+  houseNo: string;
+  lastActive: Date;
+  roadNo: string;
+  updatedAt: Date;
+  zip: string;
+
+
+}

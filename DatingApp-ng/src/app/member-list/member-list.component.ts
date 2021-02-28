521559a633cee244737b3ebe29855c8e8fe0b5f3
xahir509 <xahirahmed3@gmail.com>
2021-03-01 00:52:39 +0600

    show card view for member list app

--- DatingApp-ng/src/app/member-list/member-list.component.ts
+++ DatingApp-ng/src/app/member-list/member-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_services/DatingApp/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { User } from '../_guards/_models/user';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+  userList: User[];
 
-  constructor() { }
+  constructor(
+    private _userService:UserService,
+    private alertify: AlertifyService
+  ) { }
 
   ngOnInit(): void {
+    this.loadUser();
+
   }
+
+private loadUser(): void {
+  this._userService.getUsers().subscribe((data:User[])=>{
+    this.userList = data
+  }, error => {
+    this.alertify.error(error);
+  });
+}
 
 }

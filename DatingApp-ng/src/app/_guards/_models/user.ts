5680195fb9d73e20677eebbd281735930342681d
xahir509 <xahirahmed3@gmail.com>
2021-03-01 00:53:10 +0600

    add prop in user model

--- DatingApp-ng/src/app/_guards/_models/user.ts
+++ DatingApp-ng/src/app/_guards/_models/user.ts
@@ -5,6 +5,7 @@ import { Photo } from './photo';
 export class User implements ILog,IAddress{
   id:number;
   username:string;
+  password:string;
   knownAs:string;
   age:number;
   gender:number;
